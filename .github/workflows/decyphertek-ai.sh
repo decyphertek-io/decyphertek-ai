836cf4d02e9eaace42ec31fc6ca014022233e1e9
decyphertek-io <decyphertek-io@users.noreply.github.com>
2025-10-04 16:52:21 -0500

    decyphertek-io

--- .github/workflows/decyphertek-ai.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt install -y gh
-gh auth login
-gh workflow list
-cd /home/adminotaur/Documents/git/flet/decyphertek-ai/mobile-app/custom
-gh workflow run decyphertek-ai.yml --ref main
-gh run list --workflow=decyphertek-ai.yml
